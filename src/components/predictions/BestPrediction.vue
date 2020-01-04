ed4d7af714b6b87e7dac28abb23544a6c270b408
Brock <brock.reece@croud.co.uk>
2020-01-04 20:21:00 +0000

    Component to show the best prediction

---
+++ src/components/predictions/BestPrediction.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>{{ bestPrediction.className }}: {{ bestPrediction.probability.toFixed(2) }}</div>
+</template>
+
+<script>
+export default {
+    props: {
+        predictions: {
+            type: Array,
+            required: true
+        }
+    },
+
+    computed: {
+        bestPrediction() {
+            return this.predictions.reduce(
+                (agg, prediction) => {
+                    return prediction.probability > agg.probability ? prediction : agg;
+                },
+                { probability: 0 }
+            );
+        }
+    }
+};
+</script>
